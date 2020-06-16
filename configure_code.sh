5b4c33ef43f4c4257e6b8cbe2a0f19a8b1bcbeaf
ACvijic <aleksa.cvijic@cubes.rs>
2020-06-16 12:33:18 +0200

    feat(vscode): cp keybindings

--- configure_code.sh
+++ configure_code.sh
@@ -6,6 +6,7 @@ echo "Installing extensions"
 while read line; do code --install-extension "$line";done < $dir/.config/Code/User/extensions.txt
 echo "...done"
 
-echo "Copying settings"
+echo "Copying settings & keybindings"
 cp $dir/.config/Code/User/settings.json ~/.config/Code/User/settings.json
+cp $dir/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
 echo "...done"
